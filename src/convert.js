456208974a754a6afed0836ae567060582776a5f
Eric Siegel <siegel.eric@gmail.com>
2017-07-31 16:58:40 -0700

    Readd unsafe unicode literals.

--- src/convert.js
+++ src/convert.js
@@ -30,6 +30,15 @@ const convertInternal = function(sourcePath, dstPath, prettierBool=false, classi
     dstJS = fs.readFileSync(input.name).toString();
   }
 
+  //Gross hack to replace \xb5 or \u00b5 unicode literals into unsafe chars.
+  dstJS = dstJS.replace(/\\x[a-fA-F0-9]{2}/, (match) => {
+    return eval("'" + match + "'");
+  });
+
+  dstJS = dstJS.replace(/\\u[a-fA-F0-9]{4}/, (match) => {
+    return eval("'" + match + "'");
+  });
+
   const formatConfig = {
     jsx: {
       attrsOnSameLineAsTag: false,
